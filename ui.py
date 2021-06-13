3e22cd1818a371d65f573f26b825aefbb16d63d4
Arseny Kravchenko <me@arseny.info>
2021-06-13 18:15:53 +0200

    debug

--- ui.py
+++ ui.py
@@ -1,12 +1,17 @@
 import base64
 import json
+import logging 
 import os 
-
+import sys 
 import cv2
 import numpy as np
 import requests
 import streamlit as st
 from PIL import Image
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
 def str_to_image(s: str) -> np.ndarray:
@@ -30,17 +35,23 @@ def process(uploaded_file=uploaded_file, endpoint=endpoint):
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     if st.button('Process!'):
         url = f'{HOST}/{endpoint}'
-        print(f'posting to {url}')
+        logger.info(f'Posting to {url}')
         resp = requests.post(url,
                              data=json.dumps({'auth_key': os.environ['BETTER_IMAGES_API_KEY'], 'image': img_b64}).encode(),
                              headers={'Content-type': 'application/json', 'Accept': 'text/plain'}
                              )
         result = resp.json()
-        st.write(result['message'])
-        if not result['error']:
-            st.image(
-                cv2.cvtColor(str_to_image(result['image']), cv2.COLOR_BGR2RGB),
-                caption='Processed image.', use_column_width=True)
+
+        try:
+            st.write(result['message'])
+            if not result['error']:
+                st.image(
+                    cv2.cvtColor(str_to_image(result['image']), cv2.COLOR_BGR2RGB),
+                    caption='Processed image.', use_column_width=True)
+        except Exception:
+            st.write("Something went wrong :(")
+            logger.exception(f"Response: {result}")
+
 
 
 if uploaded_file is not None:
